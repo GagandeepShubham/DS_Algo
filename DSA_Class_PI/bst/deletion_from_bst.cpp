3343b5042d40181cc8a61173d46bddcc519c0869
GagandeepShubham <64211933+GagandeepShubham@users.noreply.github.com>
2020-11-03 18:34:37 +0530

    Deletion from a Binary Search Tree.

--- DSA_Class_PI/bst/deletion_from_bst.cpp
+++ DSA_Class_PI/bst/deletion_from_bst.cpp
@@ -1,3 +1,9 @@
+/** Deletion of a node from a Binary search tree.
+  * While deletion there can be three cases
+  * case1: if the node to be deleted is a leaf node. (easy deletion)
+  * case2: if the node to be deleted has one child.  (you have to take care of the child node), make connection of child with grand parent
+  * case3: if the node to be deleted has both child. (you have to take care of both the child), find the inorder successor.
+**/ 
 #include<iostream>
 using namespace std;
 
@@ -10,55 +16,56 @@ struct node
 
 struct node *root;
 
-struct node * insert(struct node *root, int data)
+struct node * insert(struct node *root, int data)  //inserting a node in the Binary search tree
 {
-    struct node *temp = new node();
-    temp -> left = NULL;
-    temp -> right = NULL;
-    temp -> data = data;
-    if(root == NULL)root = temp;
+    struct node *temp = new node();             //create a new node
+    temp -> left = NULL;                        //assign the left child value Null
+    temp -> right = NULL;                       //assign the right child value Null
+    temp -> data = data;                        //assign the data to the node
+    if(root == NULL)root = temp;                //if the root is null then it means the newly created node is first node of the tree.
     else
     {
-        if(data < root -> data)root -> left = insert(root -> left, data);
-        else if(data >= root -> data)root -> right = insert(root -> right, data);
+        if(data < root -> data)root -> left = insert(root -> left, data);  //if node created has smaller data than root,then insert it in left subtree of root.
+        else if(data >= root -> data)root -> right = insert(root -> right, data); //if node created has larger data than root, then insert it in right subtree of root.
     }
-    return root;
+    return root;   //return the root.
 }
 
-struct node *succ(struct node *root)
+struct node *succ(struct node *root)   //function to find the inorder successor of the given node
 {
     struct node *ptr = root -> right; // you will always find the successor of the node in its right;
     if(ptr != NULL)
     {
-        while(ptr -> left != NULL) //once you go to left then keep on going left , because successor of the node will be the 
-            ptr = ptr -> left;     //left most node in the right subtree of the given node whose successor is to be found.
+        while(ptr -> left != NULL) //once you go to left then keep on going left , because inorder successor of a node is the smallest node in its
+            ptr = ptr -> left;     //right subtree.
     }
-    return ptr;
+    return ptr;                    //return the pointer to the inorder successor
 }
 void delete_bst(struct node *root, int data)
 {
     //find the location of the node:
-    struct node *ptr = root, *parent;
-    int flag = false, tcase;
+    struct node *ptr = root, *parent;   //ptr is a pointer to the node which is to be deleted(we have to find where it is), parent pointer is a pointer to the parent
+                                        //- of that node
+    int flag = false, tcase;            //the flag will be true when we find the node which is to be deleted
     while(ptr != NULL && flag == false)
     {
-        if(data < ptr -> data)
+        if(data < ptr -> data)          //data is lesser than current ptr data
         {
-            parent = ptr;
-            ptr = ptr -> left;
+            parent = ptr;               //parent gets the address which current ptr is holding
+            ptr = ptr -> left;          //ptr moves to left
         }
-        else if(data > ptr -> data)
+        else if(data > ptr -> data)     //if data is more than the current ptr data
         {
-            parent = ptr;
-            ptr = ptr -> right;
+            parent = ptr;               //parent gets the address which current ptr is holding
+            ptr = ptr -> right;         //ptr moves to right
         }
-        else if(data == ptr -> data)
+        else if(data == ptr -> data)    //if data matches with the data of the current data, then we have found the node to be deleted.
         {
-            flag = true;
-            break;
+            flag = true;                //make flag true
+            break;                      //exit while loop
         }
     }
-    if(!flag)
+    if(!flag)                           //flag will remaing false only if the node to be deleted is not present in BST
     {
         cout << "Data " << data << " does'nt exists, no deletion!";
         return;
@@ -118,7 +125,7 @@ void delete_bst(struct node *root, int data)
 
 }
 
-void inorder(struct node *root)
+void inorder(struct node *root)  //print the BST by inorder traversal. [it will give increasing sorted order for BST]
 {
     if(root == NULL)return;
     inorder(root -> left);
@@ -132,19 +139,19 @@ int main()
     int i, n , data;
     cout << "Enter total size for BST:";
     cin >> n;
-    for(i = 0; i < n; i++)
+    for(i = 0; i < n; i++)          //insert data in the BST
     {
         cin >> data;
         root = insert(root, data);
     }
 
     cout << "BST is: ";
-    inorder(root);
+    inorder(root);         //print the current BST
 
     cout << "\nEnter the data to be deleted from BST: ";
     cin >> data;
     delete_bst(root, data);
 
     cout << "\nThe BST is: ";
-    inorder(root);
+    inorder(root);          //print the BST after the deletion of the node
 }
